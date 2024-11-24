0e9cfd38dbc53d901ecd34bf72123cd4d0467d11
Nikita Korovin <kit25kor2005@gmail.com>
2024-11-24 12:54:01 +0300

    added output of 6 cards per page

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
-
+const template = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+const cardList = document.querySelector('.places__list');
+function createCard(data) {
+  const cardElement = template.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
 
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+function renderCards() {
+    initialCards.forEach((cardData) => {
+      const card = createCard(cardData);
+      cardList.append(card);
+    });
+  }
+  
+  renderCards();
